5cff1c03d320e9c373d12f6be4a174c5bd19a5b0
ImDong <www@qs5.org>
2017-05-10 11:15:01 +0800

    修改 替换正则表达式

--- asset/bottom.js
+++ asset/bottom.js
@@ -1,3 +1,3 @@
 document.title = document.getElementsByTagName('h1').length < 1
     ? '王东_3年PHP开发_简历'
-    : document.getElementsByTagName('h1')[0].innerText.replace(/!"#$%&'()*+,-.\/:;<=>?@[\\]^`{|}~/g, '_');
+    : document.getElementsByTagName('h1')[0].innerText.replace(/[\s!"#\$%&'()*+,-.\/:;<=>?@\[\\\]\^`{|}~]+/g, '_');
